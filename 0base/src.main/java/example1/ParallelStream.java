75c4bbcb7166a99028a7906129048b286f56ad96
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-08-27 18:35:24 +0300

    added ParallelStream example

---
+++ 0base/src.main/java/example1/ParallelStream.java
@@ -0,0 +1,23 @@
+package example1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class  ParallelStream {
+    public static void main(String[] args) {
+
+        List<String> name = Arrays.asList("Kamren","Elwyn", "Ervin", "Greak", "Glena");
+
+        System.out.println("\nStream:\n");
+        name
+                .stream()
+                .filter(p->p.length() == 5)
+                .forEach(System.out::println);
+
+        System.out.println("\nParallel stream:\n");
+        name
+                .parallelStream()
+                .filter(p->p.length() == 5)
+                .forEach(System.out::println);
+    }
+}
