9752c32ee15293d3a2ce6ce6ccfc7c1397664c42
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-08-27 19:42:12 +0300

    delete and test new account

--- 1search_max/src.main/java/example1/App.java
+++ 1search_max/src.main/java/example1/App.java
@@ -1,23 +1,7 @@
 package example1;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
 
-        List<String> name = Arrays.asList("Kamren","Elwyn", "Ervin", "Greak", "Glena");
-
-        System.out.println("\nStream:\n");
-        name
-                .stream()
-                .filter(p->p.length() == 5)
-                .forEach(System.out::println);
-
-        System.out.println("\nParallel stream:\n");
-        name
-                .parallelStream()
-                .filter(p->p.length() == 5)
-                .forEach(System.out::println);
     }
 }
