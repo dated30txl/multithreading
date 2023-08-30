fb147575a0905542904eb50a3221a2ba26be0cf0
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-08-30 23:49:08 +0300

    added ExecutorService example

---
+++ 4ExecutorService/src.main/java/example1/Executor.java
@@ -0,0 +1,33 @@
+package example1;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Executor {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        List<List<Integer>> ll = Arrays.asList(
+                Arrays.asList(1, 15, 7, 6, 4),
+                Arrays.asList(1, 5, 7, 76, 4),
+                Arrays.asList(1, 5, 97, 6, 4),
+                Arrays.asList(10, 5, 7, 46, 4),
+                Arrays.asList(1, 5, 71, 6, 41));
+        ExecutorService es = Executors.newFixedThreadPool(4);
+        List<Future<Integer>> futures = new ArrayList<>();
+
+        for (List<Integer>l1: ll){
+            Future<Integer> submit = es.submit(()-> Collections.max(l1, Integer::compare));
+            futures.add(submit);
+        }
+
+        for (Future<Integer> future: futures){
+            System.out.println(future.get());
+        }
+
+        System.exit(0);
+    }
+}
+
+
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # multithreading
 Parallel programming a selection of basic examples in JAVA
 
-- [Base](https://github.com/yourhostel/multithreading/tree/main/0base/src.main/java)
+- [Base](https://github.com/yourhostel/multithreading/tree/main/0base/src.main/java/example1)
 - [ReentrantLock](https://github.com/yourhostel/multithreading/tree/main/1ReentrantLock/src.main/java/example1)
 - [Sleep](https://github.com/yourhostel/multithreading/tree/main/2sleep/src.main/java/example1)
-- [Synchronized](https://github.com/yourhostel/multithreading/tree/main/3Synchronized/src.main/java)
+- [Synchronized](https://github.com/yourhostel/multithreading/tree/main/3Synchronized/src.main/java/example1)
+- [Executor](https://github.com/yourhostel/multithreading/tree/main/4ExecutorService/src.main/java/example1)
