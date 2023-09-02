9f7cebd510a3942c6c65143a45ddcfec7d181827
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-09-02 19:13:28 +0300

    added description to Example2

--- 0base/src.main/java/example2/Example2.java
+++ 0base/src.main/java/example2/Example2.java
@@ -5,14 +5,14 @@ public class Example2 {
     private static int a = 0, b = 0;
 
     public static void main(String[] args) throws InterruptedException {
-        // Синхронизация на уровне класса, так как у нас статический контекст
+        // Синхронізація на рівні класу, тому що у нас статичний контекст
         synchronized (Example2.class) {
             a = 0;
             b = 0;
             x = 0;
             y = 0;
         }
-
+//Ці потоки працюватимуть паралельно і можуть оновлювати змінні 'a', 'b', 'x', та 'y' у будь-якому порядку.
         Thread p = new Thread(() -> {
             a = 1;
             x = b;
